df140b3641d33cbb9b418dc74b7fb36b019963e5
Shamshod <abdushamshod@gmail.com>
2021-11-08 23:21:33 +0500

    feat: implement 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,17 @@
+const fs = require("fs");
+const path = require("path");
+const readLine = require("readline");
+const fileDir = path.join(__dirname, "files");
+const newFilePath = path.join(__dirname, "files-copy");
 
+fs.readdir(fileDir, (err,files) => {
+  files.forEach(file => {
+    fs.copyFile(fileDir+'/'+file, newFilePath+'/'+file, (err) => {
+});
+  })
+})
+
+fs.mkdir(newFilePath, { recursive: true }, (err) => {
+  console.log("create");
+});
+console.log(newFilePath);
