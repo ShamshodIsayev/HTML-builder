fc8afd7a61e8645c76cf416460b011000acc2b14
Shamshod <abdushamshod@gmail.com>
2021-11-11 23:01:35 +0500

    fix: complete 3 task

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -12,7 +12,7 @@ async function showFile() {
           result += `\x1b[31mEror!\n${err}`;
         } else if (!date.isDirectory()) {
           const extName = path.extname(files);
-          result += `\n\x1b[34m\x1b[32m${path.basename(files, extName)}\t\t\x1b[34m\x1b[32m${extName.substring(1)}\t\t\x1b[34m\x1b[32m${date.size} bytes\t\t\x1b[34m`;
+          result += `${path.basename(files, extName)}-${extName.substring(1)}-${date.size} bytes\n`;
         }
       });
     }
